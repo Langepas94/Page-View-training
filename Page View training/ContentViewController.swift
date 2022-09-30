ce0f18a9ae22eaa2b3147a364af11056f83acdb0
Artyom Tyurmorezov <artyom9494@yandex.ru>
2022-09-30 19:04:47 +0300

    Creating Shablon on Storyboard

---
+++ Page View training/ContentViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ContentViewController.swift
+//  Page View training
+//
+//  Created by Артём Тюрморезов on 30.09.2022.
+//
+
+import UIKit
+
+class ContentViewController: UIViewController {
+
+    @IBOutlet weak var pageControl: UIPageControl!
+    @IBOutlet weak var emojiLabel: UILabel!
+    @IBOutlet weak var presentText: UILabel!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
